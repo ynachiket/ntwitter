3a0d2c4fe8dbb20d3202cdeef12f5455932ba437
ynachiket <nachiket.torwekar@gmail.com>
2014-08-10 02:30:17 -0700

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 var credentials = {
-    consumer_key: 'flbc4IXqso1fz61plb0OcLO2p',
-    consumer_secret: 'uIqGFNu2DPJhJbOtLXSgXRvnCmCCkDjdJmWVNYJnUC0k2tix9D',
-    access_token_key: '139324278-eJ4yabbXsiGm9jUpTWQi8x8kdpQrNJHREjcLQgX3',
-    access_token_secret: '43lMrVoPu1hJJYxCAFH1t5wMLBvEoH7ETwtxYXalJklXH'
+    consumer_key: 'your_consumer_key',
+    consumer_secret: 'secret',
+    access_token_key: 'token',
+    access_token_secret: 'access_token'
 };
 
 module.exports = credentials;
